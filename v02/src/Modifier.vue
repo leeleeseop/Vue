074c5f590cdf1896ba822fadc7f600fff204bc6a
leeleeseop <iseobi138@gmail.com>
2025-06-10 09:33:17 +0900

    Modifier.vue

---
+++ v02/src/Modifier.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+    data(){
+        return {
+            click_yes : false
+        }
+    },
+
+    methods: {
+        createAlert() {
+            if(this.click_yes){
+                alert('경고창이 떴습니다.');
+                this.click_yes = false;
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+    <button v-on:click="createAlert">클릭해 보세요.</button>
+</template>
+
+<style></style>
