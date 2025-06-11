b2430fb9c58b848f379e3cf8d70857e0bdec9e1f
leeleeseop <iseobi138@gmail.com>
2025-06-11 13:40:34 +0900

    V-on 이벤트 핸들링

--- v02/src/V-on.vue
+++ v02/src/V-on.vue
@@ -3,7 +3,15 @@ export default {
     data() {
         return{
             // 초기값 0 생성
-            count: 0
+            count: 0,
+            Name: "이이섭",
+            gender: "남자"
+        }
+    },
+    methods: {
+        clickAr : function() {
+            this.Name = "이이섭";
+            this.gender = "남자";
         }
     }
 }
@@ -18,6 +26,9 @@ export default {
     <!-- v-on:mouseover="" : 마우스가 버튼 위로 올라갔을때 이벤트를 처리하는 vue디렉티브, count=0 : 마우스가
         올라와 있을때 값을 0으로 초기화 -->
     <button v-on:mouseover="count=0">0으로 초기화</button>
+
+    <!-- v-on의 축약 : v-on:click = @click -->
+    <button @click="clickAr">버튼</button>
 </template>
 
 <style>
